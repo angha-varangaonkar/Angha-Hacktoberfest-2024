2a0b1efc5118245f00c942a6d4a03e8a17ec9e10
Angha Varanagaonkar <118808719+angha-varangaonkar@users.noreply.github.com>
2024-10-01 08:55:44 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+
+  cout << "Enter an integer: ";
+  cin >> n;
+
+  if ( n % 2 == 0)
+    cout << n << " is even.";
+  else
+    cout << n << " is odd.";
+
+  return 0;
+=======
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+
+  cout << "Enter an integer: ";
+  cin >> n;
+
+  if ( n % 2 == 0)
+    cout << n << " is even.";
+  else
+    cout << n << " is odd.";
+
+  return 0;
+>>>>>>> 1b7018d398ee3c878bbc71fbcac1e3bd854c7f73
+}
